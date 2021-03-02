998951db5bbde5ae68475c7c00e82b9b2d5586a9
luismanoel12 <36317156+luismanoel12@users.noreply.github.com>
2021-03-02 09:51:16 -0300

    ajustes

--- frontend-with-vuetify/src/components/template/HelpButton.vue
+++ frontend-with-vuetify/src/components/template/HelpButton.vue
@@ -4,11 +4,16 @@
       <v-icon dark> mdi-help </v-icon>
     </v-btn>
   </div>
+
+
 </template>
 
 <script>
 export default {
   name: "HelpButton",
+   data: () => ({
+      dialog: false,
+    }),
 };
 </script>
 
--- frontend-with-vuetify/src/http/http.js
+++ frontend-with-vuetify/src/http/http.js
@@ -1,3 +1,3 @@
 module.exports = {
-    host: "http://localhost:3000"
+    host: "http://192.168.0.107:3000"
 }
